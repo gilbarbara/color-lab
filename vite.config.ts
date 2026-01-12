aa30e93bc360f26de7439b2e42a244e7df4a9f10
Gil Barbara <gilbarbara@gmail.com>
2026-01-11 23:16:45 -0300

    Add Sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import tsConfigPaths from 'vite-tsconfig-paths';
@@ -12,13 +13,19 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    sentryVitePlugin({
+      org: 'kollectiv',
+      project: 'color-lab',
+    }),
   ],
+
   server: {
     host: true,
     open: true,
     port: 3000,
     allowedHosts: true,
   },
+
   test: {
     coverage: {
       provider: 'v8',
@@ -41,5 +48,9 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./tests/__setup__/vitest.setup.ts'],
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
