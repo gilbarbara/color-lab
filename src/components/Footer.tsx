a4a8141c60def00b3ae9bfebc2b42d4d9a429a9a
Gil Barbara <gilbarbara@gmail.com>
2026-01-13 10:25:31 -0300

    Update Footer flex layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Contact from '~/components/Contact';
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center gap-2 p-4" data-uid="Footer">
+    <footer className="flex items-center justify-center gap-2 flex-wrap p-4" data-uid="Footer">
       <Contact />
       <span className="text-foreground-500">·</span>
       <a href="https://x.com/gilbarbara" rel="noopener noreferrer" target="_blank">
