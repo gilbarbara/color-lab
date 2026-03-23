b65c991c40253c17994f3c705956cd6be99d48fa
Gil Barbara <gilbarbara@gmail.com>
2026-03-23 19:06:15 -0300

    Update Appwrite API endpoint to fix cross-domain cookies
    - fix e2e mock

--- src/config/appwrite.ts
+++ src/config/appwrite.ts
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = 'https://nyc.cloud.appwrite.io/v1';
+export const API_ENDPOINT = 'https://appwrite.colormeup.co/v1';
 export const COLLECTION_ID = 'palettes';
 export const DATABASE_ID = 'color-lab';
 export const PROJECT_ID = 'color-lab';
