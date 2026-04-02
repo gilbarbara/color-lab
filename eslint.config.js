b18827cea2f24c70bdd02af0eb2cb6c2dd86aec1
Gil Barbara <gilbarbara@gmail.com>
2026-04-02 20:29:26 -0300

    Upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,26 @@
 import config from '@gilbarbara/eslint-config';
+import vitest from '@gilbarbara/eslint-config/vitest';
+import testingLibrary from '@gilbarbara/eslint-config/testing-library';
+import playwright from 'eslint-plugin-playwright';
+
 
 export default [
   ...config,
+  ...vitest,
+  ...testingLibrary,
   {
     ignores: ['dist/**', 'node_modules/**'],
   },
   {
     rules: {
       'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+    },
+  },
+  {
+    files: ['e2e/*.spec.*'],
+    ...playwright.configs['flat/recommended'],
+    rules: {
+      'testing-library/prefer-screen-queries': 'off',
     },
   },
 ];
