9a3f7b389a770ff4d220d3e66ddf357991c849c9
Gil Barbara <gilbarbara@gmail.com>
2026-05-14 12:58:49 -0300

    Update Footer styles and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router';
 import { cn } from '@heroui/react';
 
+import useTheme from '~/hooks/useTheme';
+
 import Contact from '~/components/Contact';
 
 interface FooterProps {
@@ -8,11 +10,16 @@ interface FooterProps {
 }
 
 export default function Footer({ hideBorder = false }: FooterProps) {
+  const { isDarkMode } = useTheme();
+
   return (
     <footer
-      className={cn('w-full flex items-center justify-center gap-2 flex-wrap p-4', {
-        'border-t border-default': !hideBorder,
-      })}
+      className={cn(
+        'w-full flex items-center justify-center gap-2 flex-wrap p-4 text-foreground-500',
+        {
+          'border-t border-default': !hideBorder,
+        },
+      )}
       data-testid="Footer"
     >
       <Contact />
@@ -23,8 +30,18 @@ export default function Footer({ hideBorder = false }: FooterProps) {
       <span className="text-foreground-500">·</span>
       <Link to="/terms">Terms</Link>
       <span className="text-foreground-500">·</span>
-      <a href="https://x.com/gilbarbara" rel="noopener noreferrer" target="_blank">
-        @gilbarbara
+      <a
+        className="inline-flex items-center gap-2"
+        href="https://github.com/gilbarbara/color-lab"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img
+          alt="GitHub"
+          className="size-3"
+          src={isDarkMode ? '/icons/github-dark.svg' : '/icons/github.svg'}
+        />
+        GitHub
       </a>
     </footer>
   );
